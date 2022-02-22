1dafdb69284e7b15c03f1dea907c8a4feb386796
Alexander Swensen <alex.swensen@gmail.com>
2022-02-22 14:55:04 -0600

    refactor session and get roles

---
+++ aws/copy-iam.py
@@ -0,0 +1,74 @@
+#! /usr/bin/env python
+import boto3
+from rich.console import Console
+
+console = Console()
+
+profile_name = ''
+
+def get_boto_session(profile_name):
+     return boto3.Session(profile_name=profile_name)
+
+
+def list_profiles():
+    console.print("[red]" + "Listing profiles...")
+    for profile in boto3.Session().available_profiles:
+        console.print("[green]" + profile)
+
+
+def select_profile():
+    list_profiles()
+    profiles = boto3.Session().available_profiles
+    profile_name = console.input("Select profile: ")
+    if profile_name in profiles:
+        console.print("Selected profile: " + profile_name)
+        return profile_name
+    else:
+        console.print("Profile not found, exiting.", style="bold black on red")
+        exit(1)
+        
+def get_role_names():
+    global profile_name
+    iam = get_boto_session(profile_name).client('iam')
+    roles = []
+    role_paginator = iam.get_paginator('list_roles')
+    for resp in role_paginator.paginate():
+        for role in resp['Roles']:
+            roles.append(role['RoleName'])
+    return roles
+
+
+def main():
+    global profile_name
+    profile_name = select_profile()
+
+    # get list of roles
+    roles = get_role_names()
+    for role in roles:
+        console.print("[green]" + role)
+    
+    iam = get_boto_session(profile_name).client('iam')
+
+    selected_role_string = console.input("Select role: ")
+    if selected_role_string in roles:
+        # selected_role = [role for page in pages for role in page['Roles'] if role['RoleName'] == selected_role_string][0]
+        console.print("Selected role: " + selected_role_string)
+    else:
+        console.print("Role not found, exiting.", style="bold black on red")
+        exit(1)
+
+    selected_role = iam.get_role(RoleName=selected_role_string)['Role']
+
+    # switch to another profile
+    console.print("[red]" + "Logging in to another profile...")
+    profile_name = select_profile()
+    # session = boto3.Session(profile_name=profile_name)
+    # session.client('iam').assume_role(RoleArn=selected_role['Arn'], RoleSessionName='my-session')
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        console.print("\nExiting...")
+        exit(1)
